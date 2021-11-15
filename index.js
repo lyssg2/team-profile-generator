24ebd78c0e1842193d91fa56bad65a389fcb02be
Lyss Garcia <alyssa.g200@gmail.com>
2021-11-15 15:20:49 -0800

    base questions, manager and engineer

--- index.js
+++ index.js
@@ -14,3 +14,83 @@ const Intern = require('./lib/Intern')
 
 //  Blank Team array
 var team = ['']
+
+// Questions prompts through Inquirer
+
+function addMember() {
+    inquirer.prompt([{
+                type: 'list',
+                message: "Please select team member's role",
+                choices: [
+                    "Manager",
+                    "Engineer",
+                    "Employee",
+                    "Intern"
+                ],
+                name: 'role'
+            },
+            {
+                type: 'input',
+                message: "What is this member's name?",
+                name: 'member'
+            },
+            {
+                type: 'input',
+                message: "What is this member's ID?",
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: "What is this member's email?",
+                name: 'email'
+            }
+
+        ]
+
+    ).then(({ role, member, id, email }) => {
+        if (role === "Manager") {
+            return inquirer
+                .prompt(
+                    [{
+                            type: 'input',
+                            message: 'What is your office number?',
+                            name: 'office'
+                        },
+                        {
+                            type: 'confirm',
+                            message: 'Would you like to add another member?',
+                            name: 'anotherMember',
+                            default: 'false'
+                        }
+                    ]
+                ).then(({ office, newMember }) => {
+                    Manager.push(new Manager(member, id, email, office))
+                    if (newMember) {
+                        return prompt()
+                    }
+                })
+
+        } else if (role === "Engineer") {
+            return inquirer
+                .prompt(
+                    [{
+                            type: 'input',
+                            message: 'What is your GitHub username?',
+                            type: 'github'
+                        },
+                        {
+                            type: 'confirm',
+                            message: 'Would you like to add another member?',
+                            name: 'anotherMember',
+                            default: 'false'
+                        }
+                    ]
+                ).then(({ github, newMember }) => {
+                    Engineer.push(new Manager(member, id, email, github))
+                    if (newMember) {
+                        return prompt()
+                    }
+                })
+        }
+    })
+}
